d02f4cc63b3801a3d3c2e186e52e49ab8a612394
Chetan <cbvarmamif@gmail.com>
2021-11-17 01:17:56 +0530

    git third commit

---
+++ Features/LoginHRM.feature
@@ -0,0 +1,4 @@
+Feature: OrangeHRM login
+  Scenario: Logo presence on OrangeHRM home page
+    Given When I launch chrome browser
+    When I open orange hrm homepage
---
+++ src/test/java/stepDefination/OrangeHRMSteps.java
@@ -0,0 +1,32 @@
+package stepDefination;
+
+
+
+import io.cucumber.java.en.*;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OrangeHRMSteps {
+    WebDriver driver;
+
+    @Given("When I launch chrome browser")
+    public void when_i_launch_chrome_browser() {
+        System.setProperty("webdriver.chrome.driver", "Browaser/chromedriver.exe");
+        driver = new ChromeDriver();
+        System.out.println("Changes performed for Git");
+        System.out.println("Changes performed for Git2");
+
+    }
+
+    @When("I open orange hrm homepage")
+    public void i_open_orange_hrm_homepage() {
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+    }
+
+}
+
+// System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+//        driver = new ChromeDriver();
+//  driver.get("https://opensource-demo.orangehrmlive.com/");
